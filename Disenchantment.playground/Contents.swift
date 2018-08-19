4849e3def9b68f7434cbaca0c367a3cf24403d38
Maxime BRIAND <maximebriand@MacBook-Pro-de-Maxime.local>
2018-08-20 00:18:57 +0200

    create all character types

--- Disenchantment.playground/Contents.swift
+++ Disenchantment.playground/Contents.swift
@@ -12,5 +12,29 @@ class Character {
 }
 
 class Fighter : Character {
+    init(name: String) {
+        super.init(name: name, life: 100, strong: 30)
+    }
+    
+}
+
+class Magus : Character {
+    init(name: String) {
+        super.init(name: name, life: 70, strong: 25)
+    }
+    
+}
+
+class Colossus : Character {
+    init(name: String) {
+        super.init(name: name, life: 300, strong: 10)
+    }
+    
+}
+
+class Dward : Character {
+    init(name: String) {
+        super.init(name: name, life: 80, strong: 45)
+    }
     
 }
