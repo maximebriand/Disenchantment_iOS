00417a1e9348f4725b956a879a8ee7883d99a884
Maxime BRIAND <maximebriand@MacBook-Pro-de-Maxime.local>
2018-08-20 16:24:21 +0200

    add weapon from an enum with a switch

--- Disenchantment.playground/Contents.swift
+++ Disenchantment.playground/Contents.swift
@@ -2,11 +2,24 @@ class Character {
     let name: String
     var life: Int
     var strong: Int
+    var weapon: Weapon
     
-    init (name: String, life: Int, strong: Int){
+    init (name: String, life: Int, strong: Int, weapon: WeaponEnum){
         self.name = name
         self.life = life
         self.strong = strong
+        
+        switch weapon {
+            case WeaponEnum.sword:
+                self.weapon = Weapon(weaponName: "Epée", weaponType: ArenaType.basic, weaponPower: 0)
+            case WeaponEnum.ax:
+                self.weapon = Weapon(weaponName: "Hache", weaponType: ArenaType.basic, weaponPower: 0)
+            case WeaponEnum.wand:
+                self.weapon = Weapon(weaponName: "Baguette Magique", weaponType: ArenaType.basic, weaponPower: 0)
+            case WeaponEnum.none:
+                self.weapon = Weapon(weaponName: "Mains nues", weaponType: ArenaType.basic, weaponPower: 0)
+        }
+        
     }
     
     public func attack(opponent: Character) -> String{
@@ -16,16 +29,38 @@ class Character {
     }
 }
 
+enum ArenaType {
+    case fire, water, plant, basic
+}
+
+class Weapon {
+    let name: String
+    let type: ArenaType
+    let power: Int
+    
+    init(weaponName: String, weaponType: ArenaType, weaponPower: Int) {
+        self.name = weaponName
+        self.type = weaponType
+        self.power = weaponPower
+    }
+    
+    
+}
+
+enum WeaponEnum {
+    case sword, ax, wand, none
+}
+
 class Fighter : Character {
     init(name: String) {
-        super.init(name: name, life: 100, strong: 30)
+        super.init(name: name, life: 100, strong: 30, weapon: WeaponEnum.sword)
     }
     
 }
 
 class Magus : Character {
     init(name: String) {
-        super.init(name: name, life: 70, strong: 25)
+        super.init(name: name, life: 70, strong: 25, weapon: WeaponEnum.wand)
     }
     
     public override func attack(opponent: Character) -> String {
@@ -43,14 +78,14 @@ class Magus : Character {
 
 class Colossus : Character {
     init(name: String) {
-        super.init(name: name, life: 300, strong: 10)
+        super.init(name: name, life: 300, strong: 10, weapon: WeaponEnum.none)
     }
     
 }
 
 class Dward : Character {
     init(name: String) {
-        super.init(name: name, life: 80, strong: 45)
+        super.init(name: name, life: 80, strong: 45, weapon: WeaponEnum.ax)
     }
     
 }
@@ -58,8 +93,9 @@ class Dward : Character {
 
 var jonathan = Colossus(name: "Jonathan")
 var charles = Dward(name: "Charles")
-
+var yohann = Magus(name: "Yohann")
 jonathan.attack(opponent: charles)
 charles.life
-
-
+yohann.treat(teammate: charles)
+charles.life
+charles.weapon.name
